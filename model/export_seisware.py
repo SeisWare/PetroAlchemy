b1fde3cc56e173751dd0ac1346c3dda465374849
markogauk <markogauk@hotmail.com>
2021-03-25 13:04:29 -0600

    added seisware interactions

---
+++ model/export_seisware.py
@@ -0,0 +1,18 @@
+import SeisWare
+import pandas as pd
+import numpy as np
+
+def export_EUR_to_SeisWare(eur_value):
+
+    """
+    After EUR values are calculated in plot_decline_curve.py, send EUR values
+    as Formation Top(?) back to SeisWare.
+    """
+
+    if login_instance != None:
+        #
+
+    else:
+        # Display error message
+
+    return None
